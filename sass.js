2f33ffb132e1f21603b799067f135dc04d4d015f
bnjmnrsh <bnjmnrsh@gmail.com>
2021-03-30 15:56:11 +0100

    reneable css sourcemaps external/embeded

--- sass.js
+++ sass.js
@@ -9,10 +9,11 @@ var configs = {
 	files: ['main.scss'],
 	pathIn: 'src/scss',
 	pathOut: 'dist/css',
-	sourceMap: false,
 	indentType: 'tab',
 	indentWidth: 1,
-	minify: true
+	minify: true,
+    sourceMap: false,
+    sourceMapEmbed: false
 };
 
 // Banner
@@ -23,11 +24,35 @@ var getOptions = function (file, filename, minify) {
 		file: `${configs.pathIn}/${file}`,
 		outFile: `${configs.pathOut}/${filename}`,
 		sourceMap: configs.sourceMap,
+        sourceMapEmbed: configs.sourceMapEmbed,
 		indentType: configs.indentType,
 		indentWidth: configs.indentWidth,
 		outputStyle: minify ? 'compressed' : 'expanded'
 	};
 };
+
+var writeFile = function (pathOut, fileName, fileData) {
+    // Create the directory path
+    fs.mkdir(pathOut, { recursive: true }, function (err) {
+        // If there's an error, throw it
+        if (err) throw err
+
+        // Write the file to the path
+        fs.writeFile(`${pathOut}/${fileName}`, fileData, function (err) {
+            if (err) throw err
+
+            var data = fs.readFileSync(`${pathOut}/${fileName}`)
+            var fd = fs.openSync(`${pathOut}/${fileName}`, 'w+')
+            var insert = new Buffer.from(banner + '\n')
+            fs.writeSync(fd, insert, 0, insert.length, 0)
+            fs.writeSync(fd, data, 0, data.length, insert.length)
+            fs.close(fd, function (err) {
+                if (err) throw err
+                console.error(`Compiled ${pathOut}/${fileName}`)
+            })
+        })
+    })
+}
 
 var parseSass = function (file, minify) {
 	var filename = `${file.slice(0, file.length - 5)}${minify ? '.min' : ''}.css`;
@@ -36,35 +61,31 @@ var parseSass = function (file, minify) {
 		// If there's an error, throw it
 		if (err) throw err;
 
-		// Create the directory path
-		fs.mkdir(configs.pathOut, { recursive: true }, function (err) {
-
-			// If there's an error, throw it
-			if (err) throw err;
-
-			// Write the file to the path
-			fs.writeFile(`${configs.pathOut}/${filename}`, result.css, function (err){
-				if (err) throw err;
-
-				var data = fs.readFileSync(`${configs.pathOut}/${filename}`);
-				var fd = fs.openSync(`${configs.pathOut}/${filename}`, 'w+');
-				var insert = new Buffer.from(banner + "\n");
-				fs.writeSync(fd, insert, 0, insert.length, 0);
-				fs.writeSync(fd, data, 0, data.length, insert.length);
-				fs.close(fd, function (err) {
-					if (err) throw err;
-					console.log(`Compiled ${configs.pathOut}/${filename}`);
-				});
-			});
-
-		});
+        // Write the file
+        writeFile(configs.pathOut, filename, result.css)
 
 	});
 };
+
+var parseSourceMaps = function (file) {
+    var filename = `${file.slice(0, file.length - 5)}.css.map`
+    sass.render(getOptions(file, filename), function (err, result) {
+
+        // If there's an error, throw it
+        if (err) throw err
+
+        // Write the file
+        writeFile(configs.pathOut, filename, result.map)
+
+    })
+}
 
 configs.files.forEach(function (file) {
 	parseSass(file);
 	if (configs.minify) {
 		parseSass(file, true);
 	}
+    if (configs.sourceMap && !configs.sourceMapEmbed) {
+        parseSourceMaps(file)
+    }
 });
